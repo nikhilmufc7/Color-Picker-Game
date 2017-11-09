16f037f2359977c4880aafdd011baa5f2249154f
Nikhil Singh <Singh.nikhil999@gmail.com>
2017-11-09 23:12:27 +0530

    add random colors function

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 =document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -26,6 +20,7 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.background = pickedColor;
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -45,4 +40,29 @@ function pickColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
+
+function generateRandomColors(num){
+	var arr = [];
+
+	for(var i =0;i<num;i++){
+		
+		arr.push(randomColor())
+
+	}
+	return arr;
+
+}
+function randomColor(){
+
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b +")";
+}
+
+
+
+
+
 
